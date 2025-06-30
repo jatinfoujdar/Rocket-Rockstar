cc83ab203115ec17a6de293ba97231412bcabf70
Jatin Foujdar <jatin321001@gmail.com>
2025-06-30 14:29:26 +0530

    Initial Commit

---
+++ Rocket-Rockstar/Rocket_RockstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rocket_RockstarApp.swift
+//  Rocket-Rockstar
+//
+//  Created by jatin foujdar on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Rocket_RockstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
